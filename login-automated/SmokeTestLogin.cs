7aa072a27bf4753327e0f2015fbdee4bd1c1d9ce
EstelaM <estelamarchesani@gmail.com>
2024-02-24 16:58:40 -0300

    add login project folder, test cases and project file

---
+++ login-automated/SmokeTestLogin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace login_automated
+{
+    internal class SmokeTestLogin
+    {
+         
+        [SetUp]
+        public void Setup()
+        {
+            DriverManager driverManager = new DriverManager(); 
+            ChromeConfig config = new ChromeConfig(); 
+
+            driverManager.SetUpDriver(config);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    
+    }
+}
