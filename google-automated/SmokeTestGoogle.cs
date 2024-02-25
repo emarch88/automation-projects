814ffdcf8e13dc495cc984e68dbdb70d5f6eb1c3
EstelaM <estelamarchesani@gmail.com>
2024-02-25 00:34:26 -0300

    add test cases to login and google projects

---
+++ google-automated/SmokeTestGoogle.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace smoketest_automated
+{
+    internal class SmokeTestGoogle
+    {
+        IWebDriver driver = new ChromeDriver(); // properties of the IWebDriver Class: URL
+        //IWebDriver driverF = new FirefoxDriver();//Firefox
+        //IWebDriver driverE = new EdgeDriver();//Edge
+
+        [SetUp]
+        public void Setup()
+        {
+            //Initializing Chrome Browser
+            DriverManager driverManager = new DriverManager(); //object created using WebDriverManager namespace;
+            ChromeConfig configChrome = new ChromeConfig(); //object created using WebDriverManager.DriverConfigs.Impl namespace;
+            //FirefoxConfig configFirefox = new FirefoxConfig(); //Firefox
+            //EdgeConfig configEdge = new EdgeConfig(); //Edge
+
+            driverManager.SetUpDriver(configChrome);
+            //driverManager.SetUpDriver(configFirefox); //Firefox
+            //driverManager.SetUpDriver(configEdge); //Edge
+
+
+
+            /* Optimized version of the code above for Chrome:
+             * new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+             */
+
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://www.google.com/";
+
+        }
+
+    }
+}
--- login-automated/SmokeTestLogin.cs
+++ login-automated/SmokeTestLogin.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,25 +9,43 @@ using System.Threading.Tasks;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
+
+
 namespace login_automated
 {
-    internal class SmokeTestLogin
+    public class SmokeTestLogin
     {
-         
+        IWebDriver driver = new ChromeDriver(); //using OpenQA.Selenium;
+
         [SetUp]
         public void Setup()
         {
-            DriverManager driverManager = new DriverManager(); 
-            ChromeConfig config = new ChromeConfig(); 
-
+            DriverManager driverManager = new DriverManager();
+            ChromeConfig config = new ChromeConfig();
             driverManager.SetUpDriver(config);
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void Test1()
+        public void PositiveLogin()
         {
-            Assert.Pass();
+            driver.Url = "https://practicetestautomation.com/practice-test-login/";
+            driver.FindElement(By.CssSelector("input[type='text']")).SendKeys("student");
+            driver.FindElement(By.Name("password")).SendKeys("Password123");
+            driver.FindElement(By.Id("submit")).Click();
+
+
+            //Verifications
+            String urlFromBrowser = driver.Url; //gets the URL from the browser
+            Assert.AreEqual("https://practicetestautomation.com/logged-in-successfully/", urlFromBrowser);
+
+            bool successMsg = driver.FindElement(By.XPath("//p[@class='has-text-align-center']/strong")).Text.Contains("Congratulations");
+            Assert.AreEqual(true, successMsg);
+
+            String hrefSignOut = driver.FindElement(By.LinkText("Log out")).GetAttribute("href");
+            Assert.AreEqual("https://practicetestautomation.com/practice-test-login/", hrefSignOut);
+
+            driver.Quit();
         }
-    
     }
 }
